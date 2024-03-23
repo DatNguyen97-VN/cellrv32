ced457dec7764b8e1e7d46aba2967122d8a57359
1st-Third-Reich <thanhdat361997@gmail.com>
2024-03-23 20:30:46 +0700

    upload bus module

---
+++ rtl/core/cellrv32_bus_keeper.sv
@@ -0,0 +1,160 @@
+// ###########################################################################################################
+// # << CELLRV32 - Bus Keeper (BUSKEEPER) >>                                                                 #
+// # ********************************************************************************************************#
+// # This unit monitors the processor-internal bus. If the accessed module does not respond within           #
+// # the defined number of cycles (systemVerilog package: max_proc_int_response_time_c) or issues an ERROR   #
+// # condition, the BUS KEEPER asserts the error signal to inform the CPU.                                   #
+// # ********************************************************************************************************#
+`ifndef  _INCL_DEFINITIONS
+  `define _INCL_DEFINITIONS
+  import cellrv32_package::*;
+`endif // _INCL_DEFINITIONS
+
+module neorv32_bus_keeper (
+    // bus monitoring
+    input  logic [31:0] bus_addr_i, // address
+    input  logic        bus_rden_i, // read enable
+    input  logic        bus_wren_i, // write enable
+    input  logic        bus_ack_i,  // transfer acknowledge from bus system
+    input  logic        bus_err_i,  // transfer error from bus system
+    input  logic        bus_tmo_i,  // transfer timeout (external interface)
+    input  logic        bus_ext_i,  // external bus access
+    input  logic        bus_xip_i,  // pending XIP access
+    // host access
+    input  logic        clk_i,      // global clock line
+    input  logic        rstn_i,     // global reset, low-active, async
+    input  logic [31:0] addr_i,     // address
+    input  logic        rden_i,     // read enable
+    input  logic        wren_i,     // write enable
+    input  logic [31:0] data_i,     // data in
+    output logic [31:0] data_o,     // data out
+    output logic        ack_o,      // transfer acknowledge
+    output logic        err_o       // transfer error
+);
+    // IO space: module base address 
+    localparam hi_abb_c = index_size_f(io_size_c) - 1; // high address boundary bit
+    localparam lo_abb_c = index_size_f(buskeeper_size_c); // low address boundary bit
+    
+    // Control register 
+    const int ctrl_err_type_c =  0; // r/-: error type: 0=device error, 1=access timeout
+    const int ctrl_err_flag_c = 31; // r/c: bus error encountered, sticky
+
+    // error codes 
+    const logic err_device_c  = 1'b0; // device access error
+    const logic err_timeout_c = 1'b1; // timeout error
+
+    // sticky error flags 
+    logic err_flag;
+    logic err_type;
+
+    // access control 
+    logic acc_en; // module access enable
+    logic wren;   // word write enable
+    logic rden;   // read enable
+
+    // timeout counter size 
+    localparam cnt_width_c = index_size_f(max_proc_int_response_time_c);
+
+    // controller
+    typedef struct {
+        logic pending;
+        logic [cnt_width_c-1:0] timeout;
+        logic err_type;
+        logic bus_err;
+        logic ignore;
+        logic expired;
+    } control_t;
+    control_t control;
+
+    // Sanity Check -----------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------------
+    initial begin
+        assert (max_proc_int_response_time_c >= 2) else $error("CELLRV32 PROCESSOR CONFIG ERROR! Processor-internal bus timeout <max_proc_int_response_time_c> has to >= 2.");
+    end
+
+    // Host Access ----------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------------------
+    // access control
+    assign acc_en = (addr_i[hi_abb_c:lo_abb_c] == buskeeper_base_c[hi_abb_c:lo_abb_c]) ? 1'b1 : 1'b0;
+    assign wren = acc_en & wren_i;
+    assign rden = acc_en & rden_i;
+
+    // write access
+    always_ff @(posedge clk_i or negedge rstn_i) begin
+        if (rstn_i == 1'b0) begin
+            err_flag <= 1'b0;
+            err_type <= 1'b0;
+        end else begin
+            if (control.bus_err == 1'b1) begin // sticky error flag
+                err_flag <= 1'b1;
+                err_type <= control.err_type;
+            end else if (wren || rden) begin // clear on read or write access
+                err_flag <= 1'b0;
+            end 
+        end
+    end
+
+    // read access
+    always_ff @(posedge clk_i) begin
+        ack_o <= wren | rden; // bus handshake
+        data_o <= '0;
+        if (rden) begin
+            data_o[ctrl_err_type_c] <= err_type;
+            data_o[ctrl_err_flag_c] <= err_flag;
+        end
+    end
+
+    // Monitor -----------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------------------
+    always_ff @( posedge clk_i or negedge rstn_i ) begin : keeper_control
+        if (rstn_i == 1'b0) begin
+            control.pending  <= 1'b0;
+            control.bus_err  <= 1'b0;
+            control.err_type <= 1'b0;
+            control.timeout  <= '0;
+            control.ignore   <= 1'b0;
+        end else begin
+            // defaults
+            control.bus_err <= 1'b0;
+            // IDLE
+            if (control.pending == 1'b0) begin
+                control.timeout <= (cnt_width_c)'(max_proc_int_response_time_c) - 1'b1;
+                control.ignore <= 1'b0;
+                if (bus_rden_i || bus_wren_i) begin
+                    control.pending <= 1'b1;
+                end
+            // PENDING
+            end else begin
+                // counter timer
+                if (control.expired == 1'b0) begin
+                    control.timeout <= control.timeout - 1'b1;
+                end
+                // bus keeper shall ignore internal timeout during this access (because it's "external")
+                control.ignore <= control.ignore | (bus_ext_i | bus_xip_i);
+                // response handling
+                if (bus_err_i == 1'b1) begin // error termination by bus system
+                    control.err_type <= err_device_c; // device error
+                    control.bus_err  <= 1'b1;
+                    control.pending  <= 1'b0;
+                    // valid INTERNAL access timeout
+                    // EXTERNAL access timeout
+                end else if (((control.expired == 1'b1) && (control.ignore == 1'b0)) || (bus_tmo_i == 1'b1)) begin
+                    control.err_type <= err_timeout_c; // timeout error
+                    control.bus_err  <= 1'b1;
+                    control.pending  <= 1'b0;
+                end else if (bus_ack_i == 1'b1) begin // normal termination by bus system
+                    control.err_type <= 1'b0; // don't care
+                    control.bus_err  <= 1'b0;
+                    control.pending  <= 1'b0;
+                end
+            end
+        end
+    end : keeper_control
+
+    // timeout counter expired?
+    assign control.expired = (|control.timeout == 1'b0) ? 1'b1 : 1'b0;
+
+    // signal bus error to CPU
+    assign err_o = control.bus_err;
+
+endmodule
---
+++ rtl/core/cellrv32_busswitch.sv
@@ -0,0 +1,208 @@
+// ##################################################################################################
+// # << CELLRV32 - Bus Switch >>                                                                    #
+// # ********************************************************************************************** #
+// # Allows to access a single peripheral bus ("p_bus") by two controller ports. Controller port A  #
+// # ("ca_bus") has priority over controller port B ("cb_bus").                                     #
+// # ********************************************************************************************** #
+`ifndef  _INCL_DEFINITIONS
+  `define _INCL_DEFINITIONS
+  import cellrv32_package::*;
+`endif // _INCL_DEFINITIONS
+
+module neorv32_busswitch #(
+    parameter PORT_CA_READ_ONLY = 1,
+    parameter PORT_CB_READ_ONLY = 1
+)(
+    // global control //
+    input  logic clk_i,                 // global clock, rising edge
+    input  logic rstn_i,                // global reset, low-active, async
+    // controller interface a //
+    input  logic ca_bus_priv_i,         // current privilege level
+    input  logic ca_bus_cached_i,       // set if cached transfer
+    input  logic [31:0] ca_bus_addr_i,  // bus access address
+    output logic [31:0] ca_bus_rdata_o, // bus read data
+    input  logic [31:0] ca_bus_wdata_i, // bus write data
+    input  logic [3:0]  ca_bus_ben_i,   // byte enable
+    input  logic ca_bus_we_i,           // write enable
+    input  logic ca_bus_re_i,           // read enable
+    output logic ca_bus_ack_o,          // bus transfer acknowledge
+    output logic ca_bus_err_o,          // bus transfer error
+    // controller interface b //
+    input  logic cb_bus_priv_i,         // current privilege level
+    input  logic cb_bus_cached_i,       // set if cached transfer
+    input  logic [31:0] cb_bus_addr_i,  // bus access address
+    output logic [31:0] cb_bus_rdata_o, // bus read data
+    input  logic [31:0] cb_bus_wdata_i, // bus write data
+    input  logic [3:0]  cb_bus_ben_i,   // byte enable
+    input  logic cb_bus_we_i,           // write enable
+    input  logic cb_bus_re_i,           // read enable
+    output logic cb_bus_ack_o,          // bus transfer acknowledge
+    output logic cb_bus_err_o,          // bus transfer error
+    // peripheral bus //
+    output logic p_bus_priv_o,          // current privilege level
+    output logic p_bus_cached_o,        // set if cached transfer
+    output logic p_bus_src_o,           // access source: 0 = A, 1 = B
+    output logic [31:0] p_bus_addr_o,   // bus access address
+    input  logic [31:0] p_bus_rdata_i,  // bus read data
+    output logic [31:0] p_bus_wdata_o,  // bus write data
+    output logic [3:0]  p_bus_ben_o,    // byte enable
+    output logic p_bus_we_o,            // write enable
+    output logic p_bus_re_o,            // read enable
+    input  logic p_bus_ack_i,           // bus transfer acknowledge
+    input  logic p_bus_err_i            // bus transfer error
+    );
+    
+    // access request //
+    logic ca_rd_req_buf,  ca_wr_req_buf;
+    logic cb_rd_req_buf,  cb_wr_req_buf;
+    logic ca_req_current, ca_req_pending;
+    logic cb_req_current, cb_req_pending;
+
+    // internal bus lines //
+    logic ca_bus_ack, cb_bus_ack;
+    logic ca_bus_err, cb_bus_err;
+    logic p_bus_we,   p_bus_re;
+    
+    // access arbiter //
+    typedef enum logic [3:0] {IDLE, A_BUSY, A_RETIRE, B_BUSY, B_RETIRE} arbiter_state_t;
+    typedef struct {
+        arbiter_state_t state;
+        arbiter_state_t state_nxt;
+        logic           bus_sel;
+        logic           re_trig;
+        logic           we_trig;
+    } arbiter_t;
+    arbiter_t arbiter;
+
+    // Access Arbiter ----------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------------------
+    always_ff @( posedge clk_i or negedge rstn_i ) begin : arbiter_sync
+        if (rstn_i == 1'b0) begin
+            arbiter.state <= IDLE;
+            ca_rd_req_buf <= 1'b0;
+            ca_wr_req_buf <= 1'b0;
+            cb_rd_req_buf <= 1'b0;
+            cb_wr_req_buf <= 1'b0;
+        end else begin
+            arbiter.state <= arbiter.state_nxt;
+            // port A requests //
+            ca_rd_req_buf <= (ca_rd_req_buf | ca_bus_re_i) & (~(ca_bus_err | ca_bus_ack));
+            ca_wr_req_buf <= (ca_wr_req_buf | ca_bus_we_i) & (~(ca_bus_err | ca_bus_ack)) & (PORT_CA_READ_ONLY == 1'b0);
+            // port B requests //
+            cb_rd_req_buf <= (cb_rd_req_buf | cb_bus_re_i) & (~(cb_bus_err | cb_bus_ack));
+            cb_wr_req_buf <= (cb_wr_req_buf | cb_bus_we_i) & (~(cb_bus_err | cb_bus_ack)) & (PORT_CB_READ_ONLY == 1'b0);
+        end
+    end : arbiter_sync
+
+    // any current requests? //
+    assign ca_req_current = (PORT_CA_READ_ONLY == 1'b0) ? (ca_bus_re_i | ca_bus_we_i) : ca_bus_re_i;
+    assign cb_req_current = (PORT_CB_READ_ONLY == 1'b0) ? (cb_bus_re_i | cb_bus_we_i) : cb_bus_re_i;
+
+    // any pending requests? //
+    assign ca_req_pending = (PORT_CA_READ_ONLY == 1'b0) ? (ca_rd_req_buf | ca_wr_req_buf) : ca_rd_req_buf;
+    assign cb_req_pending = (PORT_CB_READ_ONLY == 1'b0) ? (cb_rd_req_buf | cb_wr_req_buf) : cb_rd_req_buf;
+
+    // FSM //
+    always_comb begin : arbiter_comb
+        // arbiter defaults //
+        arbiter.state_nxt = arbiter.state;
+        arbiter.bus_sel   = 1'b0;
+        arbiter.we_trig   = 1'b0;
+        arbiter.re_trig   = 1'b0;
+         // state machine //
+         unique case (arbiter.state)
+            // port A or B access
+            IDLE : begin
+                // current request from port A?
+                if (ca_req_current) begin
+                    arbiter.bus_sel = 1'b0;
+                    arbiter.state_nxt = A_BUSY;
+                // pending request from port A?
+                end else if (ca_req_pending) begin
+                    arbiter.bus_sel = 1'b0;
+                    arbiter.state_nxt = A_RETIRE;
+                // current request from port B?
+                end else if (cb_req_current) begin
+                    arbiter.bus_sel = 1'b1;
+                    arbiter.state_nxt = B_BUSY;
+                // pending request from port B?
+                end else if (cb_req_pending) begin
+                    arbiter.bus_sel = 1'b1;
+                    arbiter.state_nxt = B_RETIRE;
+                end
+            end
+            // port A pending access
+            A_BUSY : begin
+                // access from port A
+                arbiter.bus_sel = 1'b0;
+                if (p_bus_err_i || p_bus_ack_i) begin
+                    arbiter.state_nxt = IDLE;
+                end
+            end
+            // retire port A pending access
+            A_RETIRE : begin
+                arbiter.bus_sel   = 1'b0; // access from port A
+                arbiter.we_trig   = ca_wr_req_buf;
+                arbiter.re_trig   = ca_rd_req_buf;
+                arbiter.state_nxt = A_BUSY;
+            end
+            // port B pending access
+            B_BUSY : begin
+                arbiter.bus_sel = 1'b1;
+                if (p_bus_ack_i || p_bus_ack_i) begin
+                    if (ca_req_pending || ca_req_current) begin // any request from B?
+                        arbiter.state_nxt = A_RETIRE;
+                    end else begin
+                        arbiter.state_nxt = IDLE;
+                    end
+                end 
+            end
+            // retire port B pending access
+            B_RETIRE : begin
+                arbiter.bus_sel   = 1'b1; // access from port B
+                arbiter.we_trig   = cb_wr_req_buf;
+                arbiter.re_trig   = cb_rd_req_buf;
+                arbiter.state_nxt = B_BUSY;
+            end
+            default: begin // undefined
+                arbiter.state_nxt = IDLE;
+            end
+         endcase
+    end : arbiter_comb
+
+    // Peripheral Bus Switch ---------------------------------------------------------------------
+    // -------------------------------------------------------------------------------------------
+    assign p_bus_addr_o = (arbiter.bus_sel == 1'b0) ? ca_bus_addr_i : cb_bus_addr_i;
+
+    assign p_bus_wdata_o = (PORT_CA_READ_ONLY == 1'b1) ? cb_bus_wdata_i :
+                           (PORT_CB_READ_ONLY == 1'b1) ? ca_bus_wdata_i :
+                           (arbiter.bus_sel == 1'b0)   ? ca_bus_wdata_i : cb_bus_wdata_i;
+
+    assign p_bus_ben_o = (PORT_CA_READ_ONLY == 1'b1) ? cb_bus_ben_i :
+                         (PORT_CB_READ_ONLY == 1'b1) ? ca_bus_ben_i :
+                         (arbiter.bus_sel == 1'b0)   ? ca_bus_ben_i : cb_bus_ben_i;
+
+    assign p_bus_cached_o = (arbiter.bus_sel == 1'b0) ? ca_bus_cached_i : cb_bus_cached_i;
+    assign p_bus_priv_o = (arbiter.bus_sel == 1'b0) ? ca_bus_priv_i : cb_bus_priv_i;
+
+    assign p_bus_we = (arbiter.bus_sel == 1'b0) ? ca_bus_we_i : cb_bus_we_i;
+    assign p_bus_re = (arbiter.bus_sel == 1'b0) ? ca_bus_re_i : cb_bus_re_i;
+    assign p_bus_we_o = p_bus_we | arbiter.we_trig;
+    assign p_bus_re_o = p_bus_re | arbiter.re_trig;
+
+    assign p_bus_src_o = arbiter.bus_sel;
+
+    assign ca_bus_rdata_o = p_bus_rdata_i;
+    assign cb_bus_rdata_o = p_bus_rdata_i;
+
+    assign ca_bus_ack     = (arbiter.bus_sel == 1'b0) ? p_bus_ack_i : 1'b0;
+    assign cb_bus_ack     = (arbiter.bus_sel == 1'b1) ? p_bus_ack_i : 1'b0;
+    assign ca_bus_ack_o   = ca_bus_ack;
+    assign cb_bus_ack_o   = cb_bus_ack; 
+
+    assign ca_bus_err     = (arbiter.bus_sel == 1'b0) ? p_bus_err_i : 1'b0;
+    assign cb_bus_err     = (arbiter.bus_sel == 1'b1) ? p_bus_err_i : 1'b0;
+    assign ca_bus_err_o   = ca_bus_err;
+    assign cb_bus_err_o   = cb_bus_err; 
+    
+endmodule
