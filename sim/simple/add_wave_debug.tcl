6b6e2d58a85cfc8286bcf10bcda6c9706db348e2
1st-Third-Reich <thanhdat361997@gmail.com>
2025-09-15 22:33:31 +0700

    algorithm is changed

--- sim/simple/add_wave_debug.tcl
+++ sim/simple/add_wave_debug.tcl
@@ -4,6 +4,9 @@ set top_level "/cellrv32_tb_simple"
 # Gets all signals of entire design
 set signals_string [find signals -r /$top_level/*]
 
+# Initialize a list to store previously added instance strings
+set added_signals_list {}
+
 # Loop through the list, modify and add wave signal
 foreach element $signals_string {
     # Remove the leading '/' and split the string into components
@@ -15,29 +18,42 @@ foreach element $signals_string {
 
     # Initialize the modified string
     set modified_string ""
+    set new_instance_string ""
 
     # Loop through the list and build the modified string
     foreach child_element $components {
         append modified_string " -group \"$child_element\""
+        append new_instance_string "/$child_element"
     }
 
     # Replace [ with \[ and ] with \] in both strings
     set modified_string [string map {"[" "\\[" "]" "\\]"} $modified_string]
-    set instance_string [string map {"[" "\\[" "]" "\\]"} $element]
+    set new_instance_string [string map {"[" "\\[" "]" "\\]"} $new_instance_string]
 
     # Debug output to check the constructed strings
     #puts "modified_string: $modified_string"
-    #puts "instance_string: $instance_string"
+    #puts "new_instance_string: $new_instance_string"
 
-    # Check if element contains '#'
-    if {[string first "#" $instance_string] == -1} {
+    # Check if element contains '#' and if it's already added
+    if {[string first "#" $new_instance_string] == -1 && [lsearch -exact $added_signals_list $new_instance_string] == -1} {
         # Add wave with modified_string settings of element signal
-        #puts "Executing eval command: add wave $modified_string $instance_string"
-        eval add wave $modified_string $instance_string
+        #puts "Executing eval command: add wave $modified_string $new_instance_string"
+        eval add wave $modified_string $new_instance_string/*
+
+        # Add this instance string to the list of added signals
+        lappend added_signals_list $new_instance_string
     } else {
-        #puts "Skipping $instance_string due to presence of '#'."
+        #puts "Skipping $new_instance_string (already added or invalid)."
     }
 }
 
+# Additional signals
+add wave -group "cellrv32_tb_simple" -group "cellrv32_top_inst" -group "cellrv32_cpu_inst" -group "cellrv32_cpu_regfile_inst" /cellrv32_tb_simple/cellrv32_top_inst/cellrv32_cpu_inst/cellrv32_cpu_regfile_inst/reg_file
+add wave -group "cellrv32_tb_simple" -group "cellrv32_top_inst" -group "cellrv32_cpu_inst" -group "cellrv32_cpu_regfile_inst" /cellrv32_tb_simple/cellrv32_top_inst/cellrv32_cpu_inst/cellrv32_cpu_regfile_inst/reg_file_emb
+add wave -group "cellrv32_tb_simple" -group "cellrv32_top_inst" -group "cellrv32_int_dmem_inst_ON" -group "cellrv32_int_dmem_inst" /cellrv32_tb_simple/cellrv32_top_inst/cellrv32_int_dmem_inst_ON/cellrv32_int_dmem_inst/mem_ram_b0
+add wave -group "cellrv32_tb_simple" -group "cellrv32_top_inst" -group "cellrv32_int_dmem_inst_ON" -group "cellrv32_int_dmem_inst" /cellrv32_tb_simple/cellrv32_top_inst/cellrv32_int_dmem_inst_ON/cellrv32_int_dmem_inst/mem_ram_b1
+add wave -group "cellrv32_tb_simple" -group "cellrv32_top_inst" -group "cellrv32_int_dmem_inst_ON" -group "cellrv32_int_dmem_inst" /cellrv32_tb_simple/cellrv32_top_inst/cellrv32_int_dmem_inst_ON/cellrv32_int_dmem_inst/mem_ram_b2
+add wave -group "cellrv32_tb_simple" -group "cellrv32_top_inst" -group "cellrv32_int_dmem_inst_ON" -group "cellrv32_int_dmem_inst" /cellrv32_tb_simple/cellrv32_top_inst/cellrv32_int_dmem_inst_ON/cellrv32_int_dmem_inst/mem_ram_b3
+add wave -group "cellrv32_tb_simple" -group "cellrv32_top_inst" /cellrv32_tb_simple/cellrv32_top_inst/resp_bus
 # Output the completion message
 puts "Added waves for signals in entire design"
