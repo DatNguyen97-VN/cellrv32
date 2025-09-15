8d3405a0399627a4b78ca4514149314581c5f2f4
1st-Third-Reich <thanhdat361997@gmail.com>
2025-09-15 22:36:04 +0700

    updated the package path

--- sim/simple/questa_setup.sh
+++ sim/simple/questa_setup.sh
@@ -18,7 +18,7 @@ vmap work work
 
 # Compile the package and sv files
 vlog  -sv -mfcu -work cellrv32 \
-      +incdir+"$CELLV32_LOCAL_RTL"/core/packages \
+      "$CELLV32_LOCAL_RTL"/core/packages/*.svh \
       "$CELLV32_LOCAL_RTL"/core/*.sv \
       "$CELLV32_LOCAL_RTL"/core/mem/*.sv \
       ../*.sv
