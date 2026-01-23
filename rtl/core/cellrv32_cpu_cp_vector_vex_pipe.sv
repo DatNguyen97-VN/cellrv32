bb5a55438329f60339ce7f5c4028966f9085e8f9
1st-Third-Reich <thanhdat361997@gmail.com>
2026-01-23 20:00:58 +0700

    minor edits.

--- rtl/core/cellrv32_cpu_cp_vector_vex_pipe.sv
+++ rtl/core/cellrv32_cpu_cp_vector_vex_pipe.sv
@@ -10,98 +10,95 @@
 `endif // _INCL_DEFINITIONS
 
 module vex_pipe #(
-    parameter int VECTOR_REGISTERS   = 32,
-    parameter int DATA_WIDTH         = 32,
-    parameter int MICROOP_WIDTH      = 7 ,
-    parameter int VECTOR_LANES       = 8 ,
-    parameter int VECTOR_LANE_NUM    = 1 ,
-    parameter     VECTOR_FP_ALU      = 1 ,
-    parameter     VECTOR_FXP_ALU     = 0
+    parameter int XLEN            = 32,
+    parameter int VECTOR_LANES    = 8 ,
+    parameter int VECTOR_LANE_NUM = 1 ,
+    parameter     VECTOR_FP_ALU   = 1 ,
+    parameter     VECTOR_FXP_ALU  = 0
 ) (
-    input  logic                                           clk           ,
-    input  logic                                           rst_n         ,
+    input  logic            clk           ,
+    input  logic            rst_n         ,
     //Issue Interface
-    input  logic                                           valid_i       ,
-    output logic                                           fp_valid_o    ,
-    input  logic                                           done_i        ,
-    output logic                                           ready_o       ,
-    input  logic                                           mask_i        ,
-    input  logic [                         DATA_WIDTH-1:0] data_a_i      ,
-    input  logic [                         DATA_WIDTH-1:0] data_b_i      ,
-    input  logic [                                    5:0] funct6_i      ,
-    input  logic [                                    2:0] funct3_i      ,
-    input  logic [                                    2:0] frm_i         ,
-    input  logic [                                    4:0] vfunary_i     ,
-    input  logic [                                    6:0] vl_i          ,
-    input  logic                                           is_rdc_i      ,
+    input  logic            valid_i       ,
+    output logic            fp_valid_o    ,
+    input  logic            done_i        ,
+    output logic            ready_o       ,
+    input  logic            mask_i        ,
+    input  logic [XLEN-1:0] data_a_i      ,
+    input  logic [XLEN-1:0] data_b_i      ,
+    input  logic [     5:0] funct6_i      ,
+    input  logic [     2:0] funct3_i      ,
+    input  logic [     2:0] frm_i         ,
+    input  logic [     4:0] vfunary_i     ,
+    input  logic [     6:0] vl_i          ,
+    input  logic            is_rdc_i      ,
+    output logic [     3:0] valid_mul_div_o,
     //Writeback
-    input  logic                                           head_uop_ex4_i,
-    input  logic                                           end_uop_ex4_i ,
-    output logic                                           wr_en_o       ,
-    output logic [                         DATA_WIDTH-1:0] wr_data_o     ,
-    output logic                                           rdc_done_o    ,
+    input  logic            head_uop_ex4_i,
+    input  logic            end_uop_ex4_i ,
+    output logic            wr_en_o       ,
+    output logic [XLEN-1:0] wr_data_o     ,
+    output logic            rdc_done_o    ,
     //EX1 Reduction Tree Intf
-    input  logic [                         DATA_WIDTH-1:0] rdc_data_ex1_i,
-    output logic [                         DATA_WIDTH-1:0] rdc_data_ex1_o,
+    input  logic [XLEN-1:0] rdc_data_ex1_i,
+    output logic [XLEN-1:0] rdc_data_ex1_o,
     //EX2 Reduction Tree Intf
-    input  logic [                         DATA_WIDTH-1:0] rdc_data_ex2_i,
-    output logic [                         DATA_WIDTH-1:0] rdc_data_ex2_o,
+    input  logic [XLEN-1:0] rdc_data_ex2_i,
+    output logic [XLEN-1:0] rdc_data_ex2_o,
     //EX3 Reduction Tree Intf
-    input  logic [                         DATA_WIDTH-1:0] rdc_data_ex3_i,
-    output logic [                         DATA_WIDTH-1:0] rdc_data_ex3_o,
+    input  logic [XLEN-1:0] rdc_data_ex3_i,
+    output logic [XLEN-1:0] rdc_data_ex3_o,
     //EX4 Reduction Tree Intf
-    input  logic [                         DATA_WIDTH-1:0] rdc_data_ex4_i,
-    output logic [                         DATA_WIDTH-1:0] rdc_data_ex4_o,
-    output logic [                                    4:0] pipe_fflags_o
+    input  logic [XLEN-1:0] rdc_data_ex4_i,
+    output logic [XLEN-1:0] rdc_data_ex4_o,
+    output logic [     4:0] pipe_fflags_o
 );
-    localparam int EX1_W = 4*(DATA_WIDTH+8);
-    localparam int EX2_W = 3*DATA_WIDTH    ;
-    localparam int EX3_W = 3*DATA_WIDTH    ;
-    localparam int EX4_W = DATA_WIDTH      ;
-
     //Reg Declaration
-    logic             valid_int_ex2  ;
-    logic             valid_int_ex3  ;
-    logic             valid_int_ex4  ;
-    logic [EX1_W-1:0] data_ex1       ;
-    logic [EX2_W-1:0] data_ex2       ;
-    logic [EX3_W-1:0] data_ex3       ;
-    logic [EX4_W-1:0] data_ex4       ;
-    logic [EX4_W-1:0] temp_rdc_result_ex4;
-    logic             use_temp_rdc_result;
-    logic             ready_res_ex2  ;
-    logic             ready_res_ex3  ;
-    logic             ready_res_ex4  ;
-    logic             valid_result_wr;
+    logic            valid_int_ex2  ;
+    logic            valid_int_ex3  ;
+    logic            valid_int_ex4  ;
+    logic [XLEN-1:0] data_ex1       ;
+    logic [XLEN-1:0] data_ex2       ;
+    logic [XLEN-1:0] data_ex3       ;
+    logic [XLEN-1:0] data_ex4       ;
+    logic [XLEN-1:0] temp_rdc_result_ex4;
+    logic            use_temp_rdc_result;
+    logic            ready_res_ex2  ;
+    logic            ready_res_ex3  ;
+    logic            ready_res_ex4  ;
+    logic            valid_result_wr;
 
     //Wire Declaration
-    logic             valid_int_ex1      ;
-    logic             valid_fp_ex1       ;
-    logic             ready_res_int_ex1  ;
-    logic             ready_res_int_ex2  ;
-    logic             ready_res_int_ex3  ;
-    logic             ready_res_int_ex4  ;
-    logic [EX1_W-1:0] res_int_ex1        ;
-    logic [EX2_W-1:0] res_int_ex2        ;
-    logic [EX3_W-1:0] res_int_ex3        ;
-    logic [EX4_W-1:0] res_int_ex4        ;
-    logic             ready_res_fp_ex4   ;
-    logic [EX4_W-1:0] res_fp_ex4         ;
-    logic             mask_ex2           ;
-    logic             mask_ex3           ;
-    logic             mask_ex4           ;
-    logic             mask_wr            ;
-    logic             use_reduce_tree_ex1;
-    logic             use_reduce_tree_ex2;
-    logic             use_reduce_tree_ex3;
-    logic             use_reduce_tree_ex4;
-    logic [      5:0] rdc_op_ex4;
+    logic            valid_int          ;
+    logic            valid_int_ex1      ;
+    logic            valid_fp_ex1       ;
+    logic            ready_res_int_ex1  ;
+    logic            ready_res_int_ex2  ;
+    logic            ready_res_int_ex3  ;
+    logic            ready_res_int_ex4  ;
+    logic [XLEN-1:0] res_int_ex1        ;
+    logic [XLEN-1:0] res_int_ex2        ;
+    logic [XLEN-1:0] res_int_ex3        ;
+    logic [XLEN-1:0] res_int_ex4        ;
+    logic            ready_res_fp_ex4   ;
+    logic [XLEN-1:0] res_fp_ex4         ;
+    logic            mask_ex2           ;
+    logic            mask_ex3           ;
+    logic            mask_ex4           ;
+    logic            mask_wr            ;
+    logic            use_reduce_tree_ex1;
+    logic            use_reduce_tree_ex2;
+    logic            use_reduce_tree_ex3;
+    logic            use_reduce_tree_ex4;
+    logic [     5:0] rdc_op_ex4;
 
     // FP32 ALU ready / valid
     logic             vfp32_ready;
+    logic             vint_ready;
 
-    assign ready_o       = valid_fp_ex1 ? vfp32_ready : valid_i;
-    assign valid_int_ex1 = (funct3_i == funct3_opivv_c) || (funct3_i == funct3_opivi_c) || (funct3_i == funct3_opivx_c) ||
+    assign ready_o       = valid_fp_ex1 ? vfp32_ready : vint_ready;
+    assign valid_int_ex1 = (funct3_i == funct3_opivv_c) || (funct3_i == funct3_opivi_c) || (funct3_i == funct3_opivx_c) || is_rdc_i ? valid_i : 1'b0; // integer op
+    assign valid_int     = (funct3_i == funct3_opivv_c) || (funct3_i == funct3_opivi_c) || (funct3_i == funct3_opivx_c) ||
                            (funct3_i == funct3_opmvv_c) || (funct3_i == funct3_opmvx_c) ? valid_i : 1'b0; // integer op
     assign valid_fp_ex1  = (funct3_i == funct3_opfvv_c) || (funct3_i == funct3_opfvx_c) ? valid_i : 1'b0; // floating point op
     assign use_reduce_tree_ex1 = is_rdc_i & valid_i;
@@ -110,19 +107,13 @@ module vex_pipe #(
     // Integer ALU
     //-----------------------------------------------
     cellrv32_cpu_cp_vector_vex_pipe_vint #(
-        .DATA_WIDTH        (DATA_WIDTH      ),
-        .MICROOP_WIDTH     (MICROOP_WIDTH   ),
-        .VECTOR_REGISTERS  (VECTOR_REGISTERS),
-        .VECTOR_LANES      (VECTOR_LANES    ),
-        .VECTOR_LANE_NUM   (VECTOR_LANE_NUM ),
-        .EX1_W             (EX1_W           ),
-        .EX2_W             (EX2_W           ),
-        .EX3_W             (EX3_W           ),
-        .EX4_W             (EX4_W           )
+        .XLEN            (XLEN           ),
+        .VECTOR_LANES    (VECTOR_LANES   ),
+        .VECTOR_LANE_NUM (VECTOR_LANE_NUM)
     ) cellrv32_cpu_cp_vector_vex_pipe_vint_inst (
         .clk            (clk              ),
         .rst_n          (rst_n            ),
-        .valid_i        (valid_int_ex1    ),
+        .valid_i        (valid_int        ),
         .data_a_ex1_i   (data_a_i         ),
         .data_b_ex1_i   (data_b_i         ),
         .funct6_i       (funct6_i         ),
@@ -130,6 +121,8 @@ module vex_pipe #(
         .mask_i         (mask_i           ),
         .vl_i           (vl_i             ),
         .is_rdc_i       (is_rdc_i         ),
+        .ready_o        (vint_ready       ),
+        .valid_mul_div_o(valid_mul_div_o  ),
         //Reduction Tree Inputs
         .rdc_data_ex1_i (rdc_data_ex1_i   ),
         .rdc_data_ex2_i (rdc_data_ex2_i   ),
@@ -164,13 +157,7 @@ module vex_pipe #(
     //-----------------------------------------------
     generate if (VECTOR_FP_ALU) begin : cellrv32_cpu_cp_vector_vex_pipe_vfp32_ON
         cellrv32_cpu_cp_vector_vex_pipe_vfp32 #(
-            .DATA_WIDTH        (DATA_WIDTH     ),
-            .MICROOP_WIDTH     (MICROOP_WIDTH  ),
-            .VECTOR_LANE_NUM   (VECTOR_LANE_NUM),
-            .EX1_W             (EX1_W          ),
-            .EX2_W             (EX2_W          ),
-            .EX3_W             (EX3_W          ),
-            .EX4_W             (EX4_W          )
+            .XLEN (XLEN)
         ) cellrv32_cpu_cp_vector_vex_pipe_vfp32_inst (
             .clk_i          (clk             ),
             .rstn_i         (rst_n           ),
@@ -198,10 +185,10 @@ module vex_pipe #(
             .flags_ex4_o    (pipe_fflags_o   )
         );
     end else begin : cellrv32_cpu_cp_vector_vex_pipe_vfp32_OFF
-        assign ready_res_fp_ex1 = 1'b0;
-        assign ready_res_fp_ex2 = 1'b0;
-        assign ready_res_fp_ex3 = 1'b0;
-        assign ready_res_fp_ex4 = 1'b0;
+        assign fp_valid_o       = 1'b0;
+        assign ready_res_fp_ex4 = '0;
+        assign res_fp_ex4       = '0;
+        assign pipe_fflags_o    = '0;
     end endgenerate
    
     // The Data Flops are shared between the execution
@@ -240,7 +227,7 @@ module vex_pipe #(
         if (mask_ex2 || use_reduce_tree_ex2) begin
             if (ready_res_ex2) begin
                 data_ex2 <= data_ex1;
-            end else if(valid_int_ex2) begin
+            end else if (valid_int_ex2) begin
                 data_ex2 <= res_int_ex2;
             end
         end
@@ -293,9 +280,9 @@ module vex_pipe #(
     // execute all the uops
     generate if (VECTOR_LANE_NUM == 0) begin: g_rdc_tmp_rslt
         logic temp_rdc_result_en;
-        logic [EX4_W-1:0] selected_second_operand;
-        logic [EX4_W-1:0] nxt_temp_rdc_result_ex4;
-        logic [EX4_W-1:0] nxt_tmp_rslt;
+        logic [XLEN-1:0] selected_second_operand;
+        logic [XLEN-1:0] nxt_temp_rdc_result_ex4;
+        logic [XLEN-1:0] nxt_tmp_rslt;
 
         // select second operand
         assign selected_second_operand = ready_res_ex4 ? data_ex3 : res_int_ex4;
@@ -335,7 +322,7 @@ module vex_pipe #(
                     nxt_tmp_rslt = ($signed(temp_rdc_result_ex4) > $signed(selected_second_operand)) ? temp_rdc_result_ex4 : selected_second_operand;
                 end
                 default : begin
-                    nxt_tmp_rslt = 'x;
+                    nxt_tmp_rslt = '0;
                 end
             endcase
         end
@@ -367,10 +354,12 @@ module vex_pipe #(
     always_ff @(posedge clk) begin
         if (ready_res_fp_ex4) begin
             data_ex4 <= res_fp_ex4;
-        end  else if (mask_ex4 || use_reduce_tree_ex4) begin
+        end else if (ready_res_int_ex4) begin
+            data_ex4 <= res_int_ex4;
+        end else if (mask_ex4 || use_reduce_tree_ex4) begin
             if (ready_res_ex4) begin
                 data_ex4 <= data_ex3;
-            end else if(valid_int_ex4) begin
+            end else if (valid_int_ex4) begin
                 data_ex4 <= res_int_ex4;
             end
         end
@@ -382,8 +371,8 @@ module vex_pipe #(
             mask_wr         <= 1'b1;
         end else begin
             // force writeback to happen on all elements
-            valid_result_wr <= valid_int_ex4 | ready_res_fp_ex4;
-            mask_wr         <= mask_ex4 | ready_res_fp_ex4;
+            valid_result_wr <= valid_int_ex4 | ready_res_int_ex4 | ready_res_fp_ex4;
+            mask_wr         <= mask_ex4 | ready_res_int_ex4 | ready_res_fp_ex4;
         end
     end
     //------------------------------------------------------
@@ -400,46 +389,46 @@ module vex_pipe #(
                     case (rdc_op_ex4)
                         funct6_vredsum_c : begin
                             // VRADD
-                            wr_data_o = temp_rdc_result_ex4[0 +: DATA_WIDTH] + data_a_i;
+                            wr_data_o = temp_rdc_result_ex4 + data_a_i;
                         end
                         funct6_vredand_c : begin
                             // VRAND
-                            wr_data_o = temp_rdc_result_ex4[0 +: DATA_WIDTH] & data_a_i;
+                            wr_data_o = temp_rdc_result_ex4 & data_a_i;
                         end
                         funct6_vredor_c : begin
                             // VROR
-                            wr_data_o = temp_rdc_result_ex4[0 +: DATA_WIDTH] | data_a_i;
+                            wr_data_o = temp_rdc_result_ex4 | data_a_i;
                         end
                         funct6_vredxor_c : begin
                             // VRXOR
-                            wr_data_o = temp_rdc_result_ex4[0 +: DATA_WIDTH] ^ data_a_i;
+                            wr_data_o = temp_rdc_result_ex4 ^ data_a_i;
                         end
                         funct6_vredminu_c : begin
                             // VRMINU
-                            wr_data_o = ( $unsigned(temp_rdc_result_ex4[0 +: DATA_WIDTH]) < $unsigned(data_a_i) ) ?
-                                         temp_rdc_result_ex4[0 +: DATA_WIDTH] : data_a_i;
+                            wr_data_o = ( $unsigned(temp_rdc_result_ex4) < $unsigned(data_a_i) ) ?
+                                         temp_rdc_result_ex4 : data_a_i;
                         end
                         funct6_vredmin_c : begin
                             // VRMIN
-                            wr_data_o = ( $signed(temp_rdc_result_ex4[0 +: DATA_WIDTH]) < $signed(data_a_i) ) ?
-                                         temp_rdc_result_ex4[0 +: DATA_WIDTH] : data_a_i;
+                            wr_data_o = ( $signed(temp_rdc_result_ex4) < $signed(data_a_i) ) ?
+                                         temp_rdc_result_ex4 : data_a_i;
                         end
                         funct6_vredmaxu_c : begin
                             // VRMAXU
-                            wr_data_o = ( $unsigned(temp_rdc_result_ex4[0 +: DATA_WIDTH]) > $unsigned(data_a_i) ) ?
-                                         temp_rdc_result_ex4[0 +: DATA_WIDTH] : data_a_i;
+                            wr_data_o = ( $unsigned(temp_rdc_result_ex4) > $unsigned(data_a_i) ) ?
+                                         temp_rdc_result_ex4 : data_a_i;
                         end
                         funct6_vredmax_c : begin
                             // VRMAX
-                            wr_data_o = ( $signed(temp_rdc_result_ex4[0 +: DATA_WIDTH]) > $signed(data_a_i) ) ?
-                                         temp_rdc_result_ex4[0 +: DATA_WIDTH] : data_a_i;
+                            wr_data_o = ( $signed(temp_rdc_result_ex4) > $signed(data_a_i) ) ?
+                                         temp_rdc_result_ex4 : data_a_i;
                         end
                         default : begin
                             wr_data_o = 'x;
                         end
                     endcase
                 end else begin
-                    wr_data_o = data_ex4[0 +: DATA_WIDTH] & {DATA_WIDTH{mask_wr}};
+                    wr_data_o = data_ex4 & {XLEN{mask_wr}};
                 end
             end
         end : wrb_rdc_nor_output
@@ -447,14 +436,14 @@ module vex_pipe #(
 
     generate
         if (VECTOR_LANE_NUM !=0) begin : wrb_nor_output
-            assign wr_data_o = data_ex4[0 +: DATA_WIDTH] & {DATA_WIDTH{mask_wr}};
+            assign wr_data_o = data_ex4 & {XLEN{mask_wr}};
         end : wrb_nor_output
     endgenerate
 
     // Reduction Signals
-    assign rdc_data_ex1_o = data_b_i[0 +: DATA_WIDTH] & {DATA_WIDTH{use_reduce_tree_ex1}};
-    assign rdc_data_ex2_o = data_ex1[0 +: DATA_WIDTH] & {DATA_WIDTH{use_reduce_tree_ex2}};
-    assign rdc_data_ex3_o = data_ex2[0 +: DATA_WIDTH] & {DATA_WIDTH{use_reduce_tree_ex3}};
-    assign rdc_data_ex4_o = data_ex3[0 +: DATA_WIDTH] & {DATA_WIDTH{use_reduce_tree_ex4}};
+    assign rdc_data_ex1_o = data_b_i & {XLEN{use_reduce_tree_ex1}};
+    assign rdc_data_ex2_o = data_ex1 & {XLEN{use_reduce_tree_ex2}};
+    assign rdc_data_ex3_o = data_ex2 & {XLEN{use_reduce_tree_ex3}};
+    assign rdc_data_ex4_o = data_ex3 & {XLEN{use_reduce_tree_ex4}};
 
 endmodule
