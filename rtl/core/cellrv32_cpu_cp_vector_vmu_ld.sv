e555adb669484edf30a5a5132b1d8499bbbfa043
1st-Third-Reich <thanhdat361997@gmail.com>
2026-02-02 19:32:10 +0700

    :rocket: optimize logic

--- rtl/core/cellrv32_cpu_cp_vector_vmu_ld.sv
+++ rtl/core/cellrv32_cpu_cp_vector_vmu_ld.sv
@@ -43,12 +43,12 @@ module vmu_ld_eng #(
     input  logic                                grant_i       , // Memory system grants request.
     output logic                                req_en_o      , // Send memory request when asserted.
     output logic [              ADDR_WIDTH-1:0] req_addr_o    , // Byte address for memory load.
-    output logic [      $clog2(VECTOR_LANES):0] req_ticket_o  , // Unique ticket identifying the request (row + lane ID).
+    output logic [    $clog2(VECTOR_LANES)-1:0] req_ticket_o  , // Unique ticket identifying the request (row + lane ID).
     //=======================================================
     // Incoming Data from Cache
     //=======================================================
     input  logic                                resp_valid_i  , // Memory returns valid load data.
-    input  logic [      $clog2(VECTOR_LANES):0] resp_ticket_i , // Ticket indicating which lane/row is being returned.
+    input  logic [    $clog2(VECTOR_LANES)-1:0] resp_ticket_i , // Ticket indicating which lane/row is being returned.
     input  logic [          REQ_DATA_WIDTH-1:0] resp_data_i   , // Raw memory data (up to REQ_DATA_WIDTH bits).
     //=======================================================
     // Status / Sync Interface
@@ -67,8 +67,7 @@ module vmu_ld_eng #(
     logic                                                             maxvl_reached               ;
     logic                                                             vl_reached                  ;
     logic                                                             request_ready               ;
-    logic                                                             row_0_ready                 ;
-    logic                                                             row_1_ready                 ;
+    logic                                                             row_ready                   ;
     logic [                         ADDR_WIDTH-1:0]                   current_addr                ;
     logic [                         ADDR_WIDTH-1:0]                   nxt_base_addr               ;
     logic [                         ADDR_WIDTH-1:0]                   nxt_strided_addr            ;
@@ -77,28 +76,18 @@ module vmu_ld_eng #(
     logic                                                             new_transaction_en          ;
     logic [                         ADDR_WIDTH-1:0]                   current_addr_r              ;
     logic [                         ADDR_WIDTH-1:0]                   stride_r                    ;
-    logic                                                             resp_row                    ;
-    logic [                         VECTOR_LANES:0]                   resp_elem_th                ;
-    logic        [VECTOR_LANES-1:0][DATA_WIDTH-1:0]                   scratchpad_1                ;
-    logic        [VECTOR_LANES-1:0][DATA_WIDTH-1:0]                   scratchpad_2                ;
-    logic [                    VREG_ADDR_WIDTH-1:0]                   row_0_rdst                  ;
-    logic [                    VREG_ADDR_WIDTH-1:0]                   row_1_rdst                  ;
+    logic [                       VECTOR_LANES-1:0]                   resp_elem_th                ;
+    logic        [VECTOR_LANES-1:0][DATA_WIDTH-1:0]                   scratchpad                  ;
+    logic [                    VREG_ADDR_WIDTH-1:0]                   row_rdst                    ;
     logic                                                             start_new_loop              ;
     logic [$clog2(VECTOR_REGISTERS*VECTOR_LANES):0]                   nxt_total_remaining_elements;
-    logic                                                             nxt_row                     ;
     logic [                 ELEMENT_ADDR_WIDTH-1:0]                   nxt_elem                    ;
     logic [                 ELEMENT_ADDR_WIDTH-1:0]                   current_pointer_wb_r        ;
-    logic                                                             current_row                 ;
     logic [                       VECTOR_LANES-1:0]                   nxt_pending_elem            ;
     logic [                       VECTOR_LANES-1:0]                   nxt_pending_elem_loop       ;
-    logic                                          [VECTOR_LANES-1:0] pending_elem_1              ;
-    logic                                          [VECTOR_LANES-1:0] pending_elem_2              ;
-    logic                                          [VECTOR_LANES-1:0] active_elem_1               ;
-    logic                                          [VECTOR_LANES-1:0] active_elem_2               ;
-    logic                                          [VECTOR_LANES-1:0] served_elem_1               ;
-    logic                                          [VECTOR_LANES-1:0] served_elem_2               ;
-    logic                                                             writeback_complete          ;
-    logic                                                             writeback_row               ;
+    logic                                          [VECTOR_LANES-1:0] pending_elem                ;
+    logic                                          [VECTOR_LANES-1:0] active_elem                 ;
+    logic                                          [VECTOR_LANES-1:0] served_elem                 ;
     logic [                    VREG_ADDR_WIDTH-1:0]                   current_exp_loop_r          ;
     logic [                    VREG_ADDR_WIDTH-1:0]                   rdst_r                      ;
     logic [                    VREG_ADDR_WIDTH-1:0]                   max_expansion_r             ;
@@ -112,10 +101,10 @@ module vmu_ld_eng #(
     assign is_busy_o = ~currently_idle;
 
     // current instruction finished
-    assign current_finished = ((~current_row & ~pending_elem_1[nxt_elem]) | (current_row & ~pending_elem_2[nxt_elem])) & expansion_finished & new_transaction_en;
+    assign current_finished = ~pending_elem[nxt_elem] & expansion_finished & new_transaction_en;
 
     // currently no instructions are being served
-    assign currently_idle = ~|pending_elem_1 & ~|pending_elem_2 & ~|active_elem_1 & ~|active_elem_2;
+    assign currently_idle = ~|pending_elem & ~|active_elem;
 
     assign expansion_finished = maxvl_reached | vl_reached;
     assign maxvl_reached      = (current_exp_loop_r == (max_expansion_r-1));
@@ -124,34 +113,27 @@ module vmu_ld_eng #(
     assign start_new_instruction = valid_in & ready_o & ~instr_in.reconfigure;
 
     // Start from element 0 on the next destination vreg
-    assign start_new_loop = ((~current_row & ~|pending_elem_1) | (current_row & ~|pending_elem_2)) & 
-                              ~expansion_finished & 
-                            ((~nxt_row & ~|active_elem_1) | (nxt_row & ~|active_elem_2));
+    assign start_new_loop = ~|pending_elem & ~expansion_finished & ~|active_elem;
 
     // Create the memory request control signals
     assign req_en_o      = request_ready;
     assign req_addr_o    = current_addr;
-    assign req_ticket_o  = {current_row, current_pointer_wb_r};
+    assign req_ticket_o  = current_pointer_wb_r;
 
     assign new_transaction_en = req_en_o & grant_i;
-    assign request_ready      = (~current_row & pending_elem_1[current_pointer_wb_r]) | (current_row & pending_elem_2[current_pointer_wb_r]);
+    assign request_ready      = pending_elem[current_pointer_wb_r];
 
     // Unlock register signals
-    assign unlock_en_o     = writeback_complete;
-    assign unlock_reg_a_o  = row_0_ready ? row_0_rdst : row_1_rdst;
+    assign unlock_en_o     = row_ready;
+    assign unlock_reg_a_o  = row_rdst;
 
     // Create the writeback signals for the RF
-    assign writeback_complete = row_0_ready | row_1_ready;
-    assign writeback_row      = row_0_ready ? 1'b0 : 1'b1;
-
-    assign row_0_ready = ~|(active_elem_1 ^ served_elem_1) & |active_elem_1;
-    assign row_1_ready = ~|(active_elem_2 ^ served_elem_2) & |active_elem_2;
+    assign row_ready = ~|(active_elem ^ served_elem) & |active_elem;
 
     // Output aliasing
-    assign wrtbck_en_o     = row_0_ready ? {VECTOR_LANES{writeback_complete}} & served_elem_1 :
-                                           {VECTOR_LANES{writeback_complete}} & served_elem_2;
-    assign wrtbck_data_o   = row_0_ready ? scratchpad_1 : scratchpad_2;
-    assign wrtbck_reg_o    = row_0_ready ? row_0_rdst   : row_1_rdst;
+    assign wrtbck_en_o     = {VECTOR_LANES{row_ready}} & served_elem;
+    assign wrtbck_data_o   = scratchpad;
+    assign wrtbck_reg_o    = row_rdst;
 
     //=======================================================
     // Address Generation
@@ -191,28 +173,19 @@ module vmu_ld_eng #(
     //=======================================================
     // Scratchpad maintenance
     //=======================================================
-    assign resp_row = resp_ticket_i[ELEMENT_ADDR_WIDTH];
-
     // Each request loads only 1 element
-    assign resp_elem_th = resp_ticket_i[ELEMENT_ADDR_WIDTH-1:0];
+    assign resp_elem_th = resp_ticket_i;
 
     // Store new Data
     always_ff @(posedge clk_i or negedge rstn_i) begin : scratchpad_maint
         if (!rstn_i) begin
-            scratchpad_1 <= '0;
-            scratchpad_2 <= '0;
+            scratchpad <= '0;
         end else begin
             // ========================
             // row 0 maintenance
             // ========================
-            if (resp_valid_i && !resp_row) begin
-                scratchpad_1[resp_elem_th] <= resp_data_i;
-            end
-            // ========================
-            // row 1 maintenance
-            // ========================
-            if (resp_valid_i && resp_row) begin
-                scratchpad_2[resp_elem_th] <= resp_data_i;
+            if (resp_valid_i) begin
+                scratchpad[resp_elem_th] <= resp_data_i;
             end
         end
     end : scratchpad_maint
@@ -220,18 +193,13 @@ module vmu_ld_eng #(
     // keep track of the rdst for each row
     always_ff @(posedge clk_i or negedge rstn_i) begin : keep_track_dst
         if(!rstn_i) begin
-            row_0_rdst <= 0;
-            row_1_rdst <= 0;
+            row_rdst <= 0;
         end else begin
             // row 0 maintenance
             if (start_new_instruction) begin
-                row_0_rdst <= instr_in.dst;
-            end else if (start_new_loop && !nxt_row) begin
-                row_0_rdst <= rdst_r + 1;
-            end
-            // row 1 maintenance
-            if (start_new_loop && nxt_row) begin
-                row_1_rdst <= rdst_r + 1;
+                row_rdst <= instr_in.dst;
+            end else if (start_new_loop) begin
+                row_rdst <= rdst_r + 1;
             end
         end
     end : keep_track_dst
@@ -241,20 +209,16 @@ module vmu_ld_eng #(
     //=======================================================
     assign nxt_total_remaining_elements = instr_vl_r - ((current_exp_loop_r+1)*VECTOR_LANES);
 
-    // Maintain current pointer and row
-    assign nxt_row  = ~current_row;
+    // Maintain current pointer
     assign nxt_elem = current_pointer_wb_r + 1;
     always_ff @(posedge clk_i or negedge rstn_i) begin : current_ptr
         if(!rstn_i) begin
             current_pointer_wb_r <= 0;
-            current_row          <= 0;
         end else begin
             if (start_new_instruction) begin
                 current_pointer_wb_r <= 0;
-                current_row          <= 0;
             end else if (start_new_loop) begin
                 current_pointer_wb_r <= 0;
-                current_row          <= nxt_row;
             end else if (current_finished) begin
                 current_pointer_wb_r <= 0;
             end else if (new_transaction_en) begin
@@ -282,28 +246,17 @@ module vmu_ld_eng #(
     // Store new pending states
     always_ff @(posedge clk_i or negedge rstn_i) begin : pending_status
         if (!rstn_i) begin
-            pending_elem_1 <= '0;
-            pending_elem_2 <= '0;
+            pending_elem <= '0;
         end else begin
             // ========================
             // row 0 maintenance
             // ========================
             if (start_new_instruction) begin
-                pending_elem_1 <= nxt_pending_elem;
-            end else if (start_new_loop && !nxt_row) begin
-                pending_elem_1 <= nxt_pending_elem_loop;
-            end else if (new_transaction_en && !current_row) begin // single-request
-                pending_elem_1[current_pointer_wb_r] <= 1'b0;
-            end
-            // ========================
-            // row 1 maintenance
-            // ========================
-            if(start_new_instruction) begin
-               pending_elem_2 <= 1'b0;
-            end else if (start_new_loop && nxt_row) begin
-               pending_elem_2 <= nxt_pending_elem_loop;
-            end else if (new_transaction_en && current_row) begin // single-request
-               pending_elem_2[current_pointer_wb_r] <= 1'b0;
+                pending_elem <= nxt_pending_elem;
+            end else if (start_new_loop) begin
+                pending_elem <= nxt_pending_elem_loop;
+            end else if (new_transaction_en) begin // single-request
+                pending_elem[current_pointer_wb_r] <= 1'b0;
             end
         end
     end  : pending_status
@@ -311,28 +264,14 @@ module vmu_ld_eng #(
     // Keep track of active elements
     always_ff @(posedge clk_i or negedge rstn_i) begin : active_status
         if (!rstn_i) begin
-            active_elem_1 <= '0;
-            active_elem_2 <= '0;
+            active_elem <= '0;
         end else begin
-            // ========================
-            // row 0 maintenance
-            // ========================
-            if (writeback_complete && !writeback_row) begin
-                active_elem_1 <= '0;
+            if (row_ready) begin
+                active_elem <= '0;
             end else if (start_new_instruction) begin
-                active_elem_1 <= nxt_pending_elem;
-            end else if (start_new_loop && !nxt_row) begin
-                active_elem_1 <= nxt_pending_elem_loop;
-            end
-            // ========================
-            // row 1 maintenance
-            // ========================
-            if (writeback_complete && writeback_row) begin
-                active_elem_2 <= '0;
-            end else if (start_new_instruction) begin
-                active_elem_2 <= '0;
-            end else if (start_new_loop && nxt_row) begin
-                active_elem_2 <= nxt_pending_elem_loop;
+                active_elem <= nxt_pending_elem;
+            end else if (start_new_loop) begin
+                active_elem <= nxt_pending_elem_loop;
             end
         end
     end : active_status
@@ -340,28 +279,14 @@ module vmu_ld_eng #(
     // Keep track of served elements from memory
     always_ff @(posedge clk_i or negedge rstn_i) begin : keep_track_elem
         if (!rstn_i) begin
-            served_elem_1 <= '0;
-            served_elem_2 <= '0;
+            served_elem <= '0;
         end else begin
-            // ========================
-            // row 0 maintenance
-            // ========================
             if(start_new_instruction) begin
-                served_elem_1 <= 1'b0;
-            end else if (start_new_loop && !nxt_row) begin
-                served_elem_1 <= 1'b0;
-            end else if (resp_valid_i && !resp_row) begin
-                served_elem_1[resp_elem_th] <= 1'b1;
-            end
-            // ========================
-            // row 1 maintenance
-            // ========================
-            if(start_new_instruction) begin
-                served_elem_2 <= 1'b0;
-            end else if (start_new_loop && nxt_row) begin
-                served_elem_2 <= 1'b0;
-            end else if (resp_valid_i && resp_row) begin
-                served_elem_2[resp_elem_th] <= 1'b1;
+                served_elem <= 1'b0;
+            end else if (start_new_loop) begin
+                served_elem <= 1'b0;
+            end else if (resp_valid_i) begin
+                served_elem[resp_elem_th] <= 1'b1;
             end
         end
     end : keep_track_elem
